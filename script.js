d763221c8853b9c24fd61cde27bf933f9de3817d
Sena Okumuş <100844463+senakms98@users.noreply.github.com>
2023-11-12 19:42:43 +0300

    ilk deneme

---
+++ script.js
@@ -0,0 +1,11 @@
+const burger=document.querySelector(".burger-box")
+const mobileMenu=document.querySelector(".mobile-menu")
+burger.addEventListener("click", function()
+{
+    if (mobileMenu.classList.contains("show")) {
+        mobileMenu.classList.remove("show")
+    } else {
+        mobileMenu.classList.add("show")
+    }
+}
+)
